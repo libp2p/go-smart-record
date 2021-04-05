5e085c44d645685cc0d6d9e473d8ae1c55bdfd73
Alfonso de la Rocha <adlrocha@tutamail.com>
2021-04-05 14:10:44 +0200

    Added query with Dict selectors

--- vm/vm.go
+++ vm/vm.go
@@ -2,15 +2,17 @@
 package vm
 
 import (
+	"fmt"
+
 	"github.com/libp2p/go-smart-record/ir"
 )
 
 // Machine captures the public interface of a smart record virtual machine.
 // NOTE: Keys must be the same type as ir.Record Key (at least for now)
 type Machine interface {
-	Update(k string, d ir.Dict) error        // Updates the record in key k with a dict
-	Query(selector ir.Dict) (ir.Dict, error) // Queries a record using a selector dict.
-	Get(k string) ir.Record                  // Gets the whole record stored in a key (debug purposes for now)
+	Update(k string, d ir.Dict) error                  // Updates the record in key k with a dict
+	Query(k string, selector ir.Dict) (ir.Dict, error) // Queries a record using a selector dict.
+	Get(k string) ir.Dict                              // Gets the whole record stored in a key (debug purposes for now)
 
 }
 
@@ -46,10 +48,59 @@ func (v *VM) Update(k string, d ir.Dict) error {
 	return nil
 }
 
-func Query(selector ir.Dict) error {
+// Query receives a dict selector as input and traverses the dict in the key
+// to return the corresponding values
+func (v *VM) Query(k string, selector ir.Dict) (ir.Dict, error) {
+	src := v.s[k]
+	if src == nil {
+		return ir.Dict{}, fmt.Errorf("empty key in state")
+	}
+
 	// Traverse the selector and check if it exists in the stored dict for the key
 	// If the path exists return it, if not do nothing
-	panic("Not implemented")
+	return queryDict(*src, selector)
+
+}
+
+func queryDict(src ir.Dict, selector ir.Dict) (ir.Dict, error) {
+	// Check if selector or src equals nil
+	out := ir.Dict{}
+	if src.Tag == selector.Tag {
+		out.Tag = selector.Tag
+		for _, p := range selector.Pairs {
+			switch p.Value.(type) {
+			case ir.Dict:
+				srcDict := src.Get(p.Key).(ir.Dict)
+				selectorDict := selector.Get(p.Key).(ir.Dict)
+
+				// If no pairs specified it means that the full Dict wants to be returned.
+				// For now the wildcard is an empty Node.
+				if len(selectorDict.Pairs) == 0 {
+					// Check if the Tag is equal
+					if selectorDict.Tag == srcDict.Tag {
+						out = out.CopySet(p.Key, srcDict)
+					}
+				} else {
+					// If not query the specfied Pairs in the dict.
+					tmpQuery, err := queryDict(src.Get(p.Key).(ir.Dict), selector.Get(p.Key).(ir.Dict))
+					if err != nil {
+						return ir.Dict{}, err
+					}
+					out = out.CopySet(p.Key, tmpQuery)
+
+				}
+			default:
+				value := src.Get(p.Key)
+				out = out.CopySet(p.Key, value)
+			}
+		}
+	}
+	return out, nil
+}
+
+func checkTypes(x, y ir.Node) {
+	// Check if both are the same type. This is somethign to do
+	// above to avoid panic when comparing different values.
 }
 
 // Gets the whole record stored in a key
--- vm/vm_test.go
+++ vm/vm_test.go
@@ -20,7 +20,7 @@ func TestEmptyUpdate(t *testing.T) {
 
 	vm.Update("234", in)
 	out := vm.Get("234")
-	if !ir.IsEqualDict(in, out) {
+	if !ir.IsEqual(in, out) {
 		t.Fatal("Record not updated in empty key", in, out)
 	}
 }
@@ -59,7 +59,91 @@ func TestExistingUpdate(t *testing.T) {
 		t.Fatal(err)
 	}
 	out := vm.Get("234")
-	if !ir.IsEqualDict(in, out) {
+	if !ir.IsEqual(in, out) {
 		t.Fatal("Record not updated in existing key", in, out)
+	}
+}
+
+func TestQueryWrongSelector(t *testing.T) {
+	// TODO: Make tests with incorrectly formed selectors, etc.
+}
+
+func TestQuery(t *testing.T) {
+	ctx := ir.DefaultMergeContext{}
+	vm := NewVM(ctx)
+
+	in := ir.Dict{
+		Tag: "foo",
+		Pairs: ir.Pairs{
+			ir.Pair{Key: ir.String{Value: "p1"}, Value: ir.String{Value: "ssss"}},
+			ir.Pair{Key: ir.String{Value: "p2"}, Value: ir.Dict{
+				Tag: "foo.2",
+				Pairs: ir.Pairs{
+					ir.Pair{Key: ir.String{Value: "asdf"}, Value: ir.String{Value: "asfd"}},
+				},
+			},
+			}},
+	}
+	in1 := ir.Dict{
+		Tag: "foo",
+		Pairs: ir.Pairs{
+			ir.Pair{Key: ir.String{Value: "p2"}, Value: ir.Dict{
+				Tag: "foo.2",
+				Pairs: ir.Pairs{
+					ir.Pair{Key: ir.String{Value: "asdf"}, Value: ir.String{Value: "asfd"}},
+				},
+			},
+			}},
+	}
+	in2 := ir.Dict{
+		Tag: "foo",
+		Pairs: ir.Pairs{
+			ir.Pair{Key: ir.String{Value: "p1"}, Value: ir.String{Value: "ssss"}},
+		},
+	}
+
+	selector1 := ir.Dict{
+		Tag: "foo",
+		Pairs: ir.Pairs{
+			ir.Pair{Key: ir.String{Value: "p2"}, Value: ir.Dict{Tag: "foo.2"}},
+		},
+	}
+
+	selector2 := ir.Dict{
+		Tag: "foo",
+		Pairs: ir.Pairs{
+			ir.Pair{Key: ir.String{Value: "p1"}},
+		},
+	}
+
+	// Add to the key
+	err := vm.Update("234", in)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	out, err := vm.Query("234", selector1)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if !ir.IsEqual(in1, out) {
+		t.Fatal("Error querying key", "in:", in1, "out:", out)
+	}
+
+	out, err = vm.Query("234", selector2)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if !ir.IsEqual(in2, out) {
+		t.Fatal("Error querying key", "in:", in2, "out:", out)
+	}
+
+	// Check empty selector returns nothing
+	out, err = vm.Query("234", ir.Dict{})
+	if err != nil {
+		t.Fatal(err)
+	}
+	if !ir.IsEqual(out, ir.Dict{}) {
+		t.Fatal("Error querying key", "in:", in2, "out:", out)
 	}
 }
