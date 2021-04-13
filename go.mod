8c04a52667b744fae68f4db5e396bd394c4aeff5
Petar Maymounkov <petarm@gmail.com>
2021-04-12 23:29:35 -0700

    make a pass on cid implementation

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/libp2p/go-smart-record
 go 1.14
 
 require (
+	github.com/ipfs/go-cid v0.0.7
 	github.com/ipfs/go-log v1.0.4
 	github.com/ipfs/go-log/v2 v2.0.5
 	go.uber.org/zap v1.16.0
--- ir/base/cid.go
+++ ir/base/cid.go
@@ -1,14 +1,16 @@
 package base
 
 import (
+	"fmt"
 	"io"
 
+	"github.com/ipfs/go-cid"
 	"github.com/libp2p/go-smart-record/ir"
 )
 
 // Cid is a smart node, representing a valid CID.
 type Cid struct {
-	Cid string // TODO: This should be of type cid.Cid
+	Cid cid.Cid
 
 	// User holds user fields.
 	User ir.Dict
@@ -19,7 +21,7 @@ func (c Cid) EncodeJSON() (interface{}, error) {
 }
 
 func (c Cid) Disassemble() ir.Dict {
-	return c.User.CopySetTag("cid", ir.String{c.Cid}, ir.String{c.Cid})
+	return c.User.CopySetTag("cid", ir.String{"cid"}, ir.String{c.Cid.String()})
 }
 
 func (c Cid) WritePretty(w io.Writer) error {
@@ -27,5 +29,25 @@ func (c Cid) WritePretty(w io.Writer) error {
 }
 
 func (c Cid) MergeWith(ctx ir.MergeContext, x ir.Node) (ir.Node, error) {
+	xc, ok := x.(Cid)
+	if !ok {
+		return nil, fmt.Errorf("cannot merge cid with non-cid")
+	}
+	if !c.Cid.Equals(xc.Cid) {
+		return nil, fmt.Errorf("cannot merge unequal cids")
+	}
+	u, err := ir.Merge(ctx, c.User, xc.User)
+	if err != nil {
+		return nil, fmt.Errorf("cannot merge cid user data")
+	}
+	return Cid{
+		Cid:  c.Cid,
+		User: u.(ir.Dict),
+	}, nil
+}
+
+type CidAssembler struct{}
+
+func (CidAssembler) Assemble(ctx ir.AssemblerContext, srcNode ir.Node) (ir.Node, error) {
 	panic("XXX")
 }
