d45c205d36379ba24326e2078c752a0360963699
Alfonso de la Rocha <adlrocha@tutamail.com>
2021-04-20 08:38:58 +0200

    removed Record struct from implementation

--- vm/vm.go
+++ vm/vm.go
@@ -1,11 +1,10 @@
-// Package vm defines and implements a Virtual Machine for smart records.
 package vm
 
 import (
 	"fmt"
 	"sync"
 
-	peer "github.com/libp2p/go-libp2p-peer"
+	"github.com/libp2p/go-libp2p-core/peer"
 	"github.com/libp2p/go-smart-record/ir"
 )
 
@@ -13,15 +12,10 @@ import (
 // Each peer has a private dataspace to store dicts.
 type RecordValue map[peer.ID]*ir.Dict
 
-type Record struct {
-	Key   string      // Key of the record
-	Value RecordValue // Root stores peers dictionaries in that key.
-}
-
 // Machine captures the public interface of a smart record virtual machine.
 type Machine interface {
 	Update(writer peer.ID, k string, update ir.Dict) error // Updates the dictionary in the writer's private space.
-	Get(k string) Record                                   // Get the full Record in a key
+	Get(k string) RecordValue                              // Get the full Record in a key
 	// NOTE: No query operation will be supported until we figure out selectors
 	// Query(key string, selector Selector) (RecordValue, error)
 }
@@ -30,9 +24,9 @@ type Machine interface {
 type vm struct {
 	ctx ir.UpdateContext // UpdateContext the VM uses to resolve conflicts
 	//ds  ds.Datastore    // TODO: Add a datastore instead of using map[string] for the VM state
-	keys map[string]*Record // State of the VM storing the map of records.
-	asm  ir.Assembler       // Assemble to use in the VM.
-	lk   sync.RWMutex       // Lock to enable multiple access
+	keys map[string]*RecordValue // State of the VM storing the map of records.
+	asm  ir.Assembler            // Assemble to use in the VM.
+	lk   sync.RWMutex            // Lock to enable multiple access
 }
 
 // NewVM creates a new smart record Machine
@@ -44,19 +38,32 @@ func NewVM(ctx ir.UpdateContext, asm ir.Assembler) Machine {
 func newVM(ctx ir.UpdateContext, asm ir.Assembler) *vm {
 	return &vm{
 		ctx:  ctx,
-		keys: make(map[string]*Record),
+		keys: make(map[string]*RecordValue),
 		asm:  asm,
 	}
 }
 
 // Get the whole record stored in a key
-func (v *vm) Get(k string) Record {
+func (v *vm) Get(k string) RecordValue {
 	v.lk.RLock()
 	defer v.lk.RUnlock()
+	// If nothing in key
 	if v.keys[k] == nil {
-		return Record{Key: k, Value: RecordValue{}}
+		return RecordValue{}
 	}
-	return *v.keys[k]
+
+	// Disassembles all nodes in record
+	out := make(map[peer.ID]*ir.Dict, 0)
+	for pk, v := range *v.keys[k] {
+		d := v.Disassemble()
+		do, ok := d.(ir.Dict)
+		if !ok {
+			// Do not return nodes which are not ir.Dict
+			continue
+		}
+	}
+	return out
+
 }
 
 // Update the dictionary in the writer's private space
@@ -81,7 +88,7 @@ func (v *vm) Update(writer peer.ID, k string, update ir.Dict) error {
 
 	// Directly store d if there is nothing in the key
 	if v.keys[k] == nil {
-		v.keys[k] = &Record{Key: k, Value: make(map[peer.ID]*ir.Dict, 0)}
+		v.keys[k] = make(map[peer.ID]*ir.Dict, 0)
 		v.keys[k].Value[writer] = &d
 		return nil
 	} else {
@@ -95,7 +102,7 @@ func (v *vm) Update(writer peer.ID, k string, update ir.Dict) error {
 			if err != nil {
 				return nil
 			}
-			*v.keys[k].Value[writer] = n.(ir.Dict)
+			*v.keys[k][writer] = n.(ir.Dict)
 		}
 
 	}
